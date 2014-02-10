84d3c69ca475490ae5632112e1fa67f3217bccaa
cewing <cris@crisewing.com>
2014-02-10 15:17:02 -0800

    initial commit

---
+++ README.rst
@@ -0,0 +1,67 @@
+********
+FizzBuzz
+********
+
+A pair of simple example functions that implement the fizzbuzz game.
+
+``fizzbuzz``
+============
+
+For any given 'n', print 'Fizz' if n is divisible by 3, 'Buzz' if n is
+divisible by 5, and 'FizzBuzz' if in is divisible by *both* 3 and 5. For all
+other numbers, print that number.
+
+.. code-block:: pycon
+
+
+    >>> from fizzbuzz import fizzbuzz
+    >>> for i in range(16):
+    ...     print fizzbuzz(i)
+    ...
+    0
+    1
+    2
+    Fizz
+    4
+    Buzz
+    Fizz
+    7
+    8
+    Fizz
+    Buzz
+    11
+    Fizz
+    13
+    14
+    FizzBuzz
+
+
+``fizzbuzz_extended``
+=====================
+
+Implement the FizzBuzz game, but allow arbitrary extensions.  For example,
+numbers divisible by seven should print as 'Sezz'.
+
+.. code-block:: pycon
+
+    >>> from fizzbuzz import fizzbuzz_extended
+    >>> extras = {7: 'Sezz'}
+    >>> for i in range(16):
+    ...     print fizzbuzz_extended(i, extras)
+    ...
+    0
+    1
+    2
+    Fizz
+    4
+    Buzz
+    Fizz
+    Sezz
+    8
+    Fizz
+    Buzz
+    11
+    Fizz
+    13
+    Sezz
+    FizzBuzz
---
+++ fizzbuzz.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The module provides two functions, fizzbuzz and fizzbuzz_extended
+"""
+
+
+def fizzbuzz(n):
+    """return a fizzbuzz-formatted representation of n"""
+    if n == 0:
+        return str(0)
+    out = ''
+    if not n % 3:
+        out += 'Fizz'
+    if not n % 5:
+        out += 'Buzz'
+    if not out:
+        out = str(n)
+    return out
+
+
+def fizzbuzz_extended(n, additional={}):
+    """return a fizzbuzz-formatted representation of n, with extensions
+
+    If passed, the `additional` arg should be a dictionary keyed by integer
+    with the values being the desired string substitution for that integer
+    """
+    if n == 0:
+        return str(0)
+    base = {3: 'Fizz', 5: 'Buzz'}
+    base.update(additional)
+    out = ''
+    for x in sorted(base.iterkeys()):
+        if not n % x:
+            out += base[x]
+    if not out:
+        out = str(n)
+    return out
+
+
+if __name__ == '__main__':
+    print "FizzBuzz Produces:"
+    for i in xrange(100):
+        print fizzbuzz(i),
+    print
+    print ""
+    others = {7: 'Sizz', 11: 'Elzz'}
+    for i in xrange(100):
+        print fizzbuzz_extended(i, others),
