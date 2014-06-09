640c8bbe3d1dcd79d98035983e43b39cceea852c
cewing <cris@crisewing.com>
2014-06-08 19:09:28 -0700

    fix up a bit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,13 +4,11 @@
 """
 
 
-data = ((3, 'Fizz'), (5, 'Buzz'))
-
-
 def fizzbuzz(n):
     """return a fizzbuzz-formatted representation of n"""
     if n == 0:
         return str(0)
+    data = ((3, 'Fizz'), (5, 'Buzz'))
     out = ''
     for divisor, replacement in data:
         if not n % divisor:
