65f0459e96e4ec820304af679f388d11d574ed58
cewing <cris@crisewing.com>
2014-06-08 17:28:35 -0700

    stepping back to the beginning of time

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,16 +6,7 @@
 
 def fizzbuzz(n):
     """return a fizzbuzz-formatted representation of n"""
-    if n == 0:
-        return str(0)
-    out = ''
-    if not n % 3:
-        out += 'Fizz'
-    if not n % 5:
-        out += 'Buzz'
-    if not out:
-        out = str(n)
-    return out
+    pass
 
 
 def fizzbuzz_extended(n, additional={}):
@@ -24,25 +15,4 @@ def fizzbuzz_extended(n, additional={}):
     If passed, the `additional` arg should be a dictionary keyed by integer
     with the values being the desired string substitution for that integer
     """
-    if n == 0:
-        return str(0)
-    base = {3: 'Fizz', 5: 'Buzz'}
-    base.update(additional)
-    out = ''
-    for x in sorted(base.iterkeys()):
-        if not n % x:
-            out += base[x]
-    if not out:
-        out = str(n)
-    return out
-
-
-if __name__ == '__main__':
-    print "FizzBuzz Produces:"
-    for i in xrange(100):
-        print fizzbuzz(i),
-    print
-    print ""
-    others = {7: 'Sizz', 11: 'Elzz'}
-    for i in xrange(100):
-        print fizzbuzz_extended(i, others),
+    pass
